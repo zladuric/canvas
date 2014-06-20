3a29b05ae2b2735f3704bbec800b25d4585bc036
Zlatko Đurić <zladuric@gmail.com>
2014-06-20 05:12:37 +0200

    more games

--- app.js
+++ app.js
@@ -10,33 +10,90 @@
         c.fillStyle = 'black';
         c.fillRect(0, 0, canvas.width, canvas.height);
 
+        var numOfParticles = document.getElementById('num-of-particles');
         // setup particles to show on screen
         var particles = [];
         var numParticles = 10;
         var particleIndex = 0;
+        var liveParticles = 0;
+        var gravity = 0.4;
 
         // setup Particle model
         function Particle() {
+            if(liveParticles > 50 || liveParticles < -50) {
+                return;
+            }
+            this.size = 5;
             this.x = canvas.width / 2;
-            this.y = canvas.height  / 2;
+            this.y = 11;
             this.vx = Math.random() * 10 - 5;
-            this.vy = Math.random() * 10 - 5;
-            this.bounces = 3;
+            this.vy = Math.random() * 10;
+            this.color = 'white';
+            this.bounces = 0;
+            this.maxBounces = Math.random() * 10;
+            this.id = particleIndex;
             particles[particleIndex++] = this;
+            console.log('inc liveParticles');
+            liveParticles = liveParticles + 1;
+            numOfParticles.innerHTML = liveParticles;
         }
 
         Particle.prototype.draw = function() {
             this.x += this.vx;
             this.y += this.vy;
-            c.fillStyle = 'white';
-            c.fillRect(this.x, this.y, 5, 5);
-            if (this.x >= canvas.width || this.x <= 0) {
+            this.vy = this.vy + gravity;
+            //this.vx = this.vx + (gravity / 2);
+            this.vy = this.vy * 0.98;
+            c.fillStyle = this.color;
+            c.fillRect(this.x, this.y, this.size, this.size);
+            if (this.x >= (canvas.width - 10) || this.x <= 10) {
                 this.vx = this.vx * -1;
+                this.bounces++;
+                this.color = 'rgb(' + parseInt(Math.random() * 255) + ', ' +
+                                        parseInt(Math.random() * 255) + ', ' +
+                                        parseInt(Math.random() * 255) + ')';
             }
-            if (this.y >= canvas.height || this.y <= 0) {
+            if (this.y >= (canvas.height - 10) || this.y <= 10) {
                 this.vy = this.vy * -1;
+                this.bounces++;
+            }
+
+            if(this.bounces > this.maxBounces) {
+                this.destroy();
             }
         }
+
+        Particle.prototype.destroy = function() {
+            c.fillStyle = 'red';
+            var self = this;
+
+            if (this.size < 20) {
+                setTimeout(function(){
+                    self.size++;
+                    c.fillRect(self.x, self.y, self.size, self.size);
+                    self.destroy();
+                }, 45);
+                return;
+            };
+            console.log('dec liveParticles')
+            liveParticles = liveParticles - 1;
+            delete particles[self.id];
+            var reload = Math.random();
+            console.log(reload);
+            if(reload > 0.5) {
+                console.log('new particle after old died', liveParticles);
+                new Particle();
+            } else {
+                console.log('no new particle');
+            }
+            if(reload > 0.8) {
+                console.log('second new particle');
+                new Particle();
+            }
+
+        }
+
+        // start with 10 particles
         for (var i = 0; i < numParticles; i++) {
             new Particle();
         }
